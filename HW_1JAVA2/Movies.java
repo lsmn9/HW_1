780e2cfe8385c75b53bbb106213536a59d2195c7
lsmn9 <lsmn9@mail.ru>
2020-04-26 22:46:17 +0300

    IC-2019.3.3 <lsmn@lsmn-Inspiron-15-3567 Overwrite remote /home/lsmn/HW_1 to local

--- HW_1JAVA2/Movies.java
+++
@@ -1,9 +0,0 @@
-package HW_1;
-
-public interface Movies  {
-    void run (double r);
-    void jump(double j);
-    String getName();
-    Boolean getValid();
-}
-
--- HW_2JAVA2/ArraySizeException.java
+++
@@ -1,9 +0,0 @@
-package HW_2;
-
-public class ArraySizeException extends Exception {
-    @Override
-    public String getMessage() {
-        return "Массив НЕВЕРНОГО размера";
-    }
-}
-
