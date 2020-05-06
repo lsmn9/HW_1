59e297505e294d6758eedca25a8430215587c4d0
lsmn9 <lsmn9@mail.ru>
2020-05-07 00:22:56 +0300

    Пока без графики. Сделал задачу из методички + по мелочи. Смотреть можно только файл ClientHandler. Изменения только там. Остальное Ваш код.

---
+++ JAva2/HW_7/Client2.java
@@ -0,0 +1,53 @@
+package HW_7;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client2 {
+
+    private final static String HOST = "localhost";
+
+    public static void main(String[] args) {
+        try(Socket socket = new Socket(HOST, 8189)){
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            boolean running = true;
+            Scanner cin = new Scanner(System.in);
+
+            Thread thread = new Thread(()->{
+                while (running) {
+                    String message = null;
+                    try {
+                        message = in.readUTF();
+                        if (message.equals("_exit_")) {
+                            in.close();
+                            out.close();
+                            break;
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(message);
+                }
+            });
+            thread.setDaemon(true);
+            thread.start();
+
+            while(running) {
+                String line = cin.nextLine();
+                if (line.equals("exit")) {
+                    out.writeUTF("_exit_");
+                    out.flush();
+                    break;
+                }
+                out.writeUTF(line);
+                out.flush();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
