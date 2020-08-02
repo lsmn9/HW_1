85c518415164e11ffc2a9f134424a7eff4ca1e23
lsmn9 <lsmn9@mail.ru>
2020-08-03 00:55:34 +0300

    Андройд_2.ДЗ_2.Небольшие комментрарии оставил внутри кода

--- Android1/HomeWork1/app/src/main/java/com/example/hw1/SecondActivity.java
+++ Android1/HomeWork1/app/src/main/java/com/example/hw1/SecondActivity.java
@@ -1,13 +1,18 @@
 package com.example.hw1;
 
+import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,21 +21,54 @@ import androidx.appcompat.widget.Toolbar;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 
-public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener, SensorEventListener {
 
-
+    CheckBox humbox, presbox, windbox;
+    SensorManager mySM;
+    Sensor humSens, presSens;
+    TextView humData, presData;
+    float humValue, presValue;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.city_choice);
-        Button btn = (Button) findViewById(R.id.confirmation);
+        Button btn = findViewById(R.id.confirmation);
         btn.setOnClickListener(this);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        humbox = findViewById(R.id.humidity);
+        presbox= findViewById(R.id.pressure);
+        windbox = findViewById(R.id.wind);
 
+        mySM = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        humSens = mySM.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
+        humData =  findViewById(R.id.hum_TxtV);
+        presSens = mySM.getDefaultSensor(Sensor.TYPE_PRESSURE);
+        presData = findViewById(R.id.pres_TxtV);
+    }
 
+    @Override
+    protected void onResume() {
+       super.onResume();
+       // регистрируем слушатели
+       if (presSens != null){ // на случай, если не предусмотрен
+           mySM.registerListener(this, presSens, SensorManager.SENSOR_DELAY_NORMAL);
+       } else {
+           presData.setText("No_such_sensor");}
+
+       if (humSens != null){
+           mySM.registerListener(this, humSens, SensorManager.SENSOR_DELAY_NORMAL);
+       } else {
+           humData.setText("No_such_sensor");}
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mySM.unregisterListener(this); // разрегистриуем
     }
 
     @Override
@@ -42,22 +80,17 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
-        Button button= findViewById(R.id.confirmation);
-        CheckBox hum, pres, wind;
-        hum = findViewById(R.id.humidity);
-        pres= findViewById(R.id.pressure);
-        wind = findViewById(R.id.wind);
-
+        Button button = findViewById(R.id.confirmation);
         if (id == R.id.menu_all) {
-            hum.setChecked(true);
-            pres.setChecked(true);
-            wind.setChecked(true);
+            humbox.setChecked(true);
+            presbox.setChecked(true);
+            windbox.setChecked(true);
             return true;
         }
         if (id == R.id.menu_nothing){
-            hum.setChecked(false);
-            pres.setChecked(false);
-            wind.setChecked(false);
+            humbox.setChecked(false);
+            presbox.setChecked(false);
+            windbox.setChecked(false);
         return true;}
         return super.onOptionsItemSelected(item);
     }
@@ -70,8 +103,39 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
         {startActivity(intent);}
         else Snackbar.make (view, "Wrong City", Snackbar.LENGTH_SHORT).
                 setAction("Action", null).show();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+        // если сенсор определенного типа записываем данные
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_RELATIVE_HUMIDITY)
+        {humValue = sensorEvent.values[0];}
+        if (sensorEvent.sensor.getType()==Sensor.TYPE_PRESSURE)
+        {presValue = sensorEvent.values[0];}
+        // небольшой метод для вывода
+        boxIsChoosen(humbox, String.valueOf(humValue), humData);
+        boxIsChoosen(presbox,String.valueOf(presValue), presData);
 
     }
 
+    private void boxIsChoosen (CheckBox checkBox, String string, TextView textView){
+      // при выбранном чекбоксе показываем данные
+        if (checkBox.isChecked()){
+            textView.setVisibility(View.VISIBLE);
+            textView.setText(string);
+        }
+        else { // при невыбранном скрываем поле
+            textView.setVisibility(View.INVISIBLE);
+        }
+    }
 
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
 }
