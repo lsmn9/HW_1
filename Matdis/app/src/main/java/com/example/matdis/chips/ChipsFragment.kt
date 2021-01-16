0d857ea937725dd4c85e980b8f70fd2367f01031
lsmn9 <lsmn9@mail.ru>
2021-01-16 18:15:20 +0300

    ImageView не переделывал

---
+++ Matdis/app/src/main/java/com/example/matdis/chips/ChipsFragment.kt
@@ -0,0 +1,29 @@
+package com.example.matdis.chips
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.matdis.R
+import com.google.android.material.chip.Chip
+import kotlinx.android.synthetic.main.chips_fragment.*
+
+class ChipsFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.chips_fragment, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        chipGroup.setOnCheckedChangeListener { chipGroup, position ->
+            chipGroup.findViewById<Chip>(position)?.let {
+                Toast.makeText(context, "Выбран ${it.text}", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
