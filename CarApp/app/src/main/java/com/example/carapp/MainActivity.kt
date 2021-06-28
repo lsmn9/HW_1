760246c3c76761dddddbee4febcee28fc5658c26
lsmn9 <lsmn9@mail.ru>
2021-06-29 01:20:23 +0300

    с кодом

--- CarApp/app/src/main/java/com/example/carapp/MainActivity.kt
+++ CarApp/app/src/main/java/com/example/carapp/MainActivity.kt
@@ -10,41 +10,41 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-//    private lateinit var car: CarView
-//    private val TIME_TO_MOVE = 5000L
-//    private var count =0
-//    var destinationX = 0
-//    var destinationY = 0
-//    var TURN = 180f
-//
-//    @SuppressLint("ResourceType")
-//    @RequiresApi(Build.VERSION_CODES.R)
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//        car = findViewById(R.id.car)
-//
-//        car.setOnClickListener(View.OnClickListener {
-//            if (count%2 ==0){
-//                destinationX = this.resources.displayMetrics.widthPixels
-//                destinationY = this.resources.displayMetrics.heightPixels
-//            }
-//            else {
-//                destinationX = 0
-//                destinationY = 0 }
-//
-//            it.animate()
-//                .setDuration(TIME_TO_MOVE)
-//                .x(2*destinationX.toFloat()
-//                        /this.resources.displayMetrics.density )
-//                .rotationBy(TURN)
-//                .y(2*destinationY.toFloat()
-//                        /this.resources.displayMetrics.density)
-//            count++
-//        })
-//
-//
-//    }
+    private lateinit var car: CarView
+    private val TIME_TO_MOVE = 5000L
+    private var count =0
+    var destinationX = 0
+    var destinationY = 0
+    var TURN = 180f
+
+    @SuppressLint("ResourceType")
+    @RequiresApi(Build.VERSION_CODES.R)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        car = findViewById(R.id.car)
+
+        car.setOnClickListener(View.OnClickListener {
+            if (count%2 ==0){
+                destinationX = this.resources.displayMetrics.widthPixels
+                destinationY = this.resources.displayMetrics.heightPixels
+            }
+            else {
+                destinationX = 0
+                destinationY = 0 }
+
+            it.animate()
+                .setDuration(TIME_TO_MOVE)
+                .x(2*destinationX.toFloat()
+                        /this.resources.displayMetrics.density )
+                .rotationBy(TURN)
+                .y(2*destinationY.toFloat()
+                        /this.resources.displayMetrics.density)
+            count++
+        })
+
+
+    }
 }
 
 
